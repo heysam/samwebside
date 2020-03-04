f5e8f8f775577a78e312d6d513ff8ff755f241d8
SamJhuo <heysam.0818@gmail.com>
2020-03-05 03:40:38 +0800

    wsgi

--- testsamweb/wsgi.py
+++ testsamweb/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testsamweb.settings.dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testsamweb.settings')
 
 application = Cling(get_wsgi_application())
